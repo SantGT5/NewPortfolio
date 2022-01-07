fdef4e86309a39af582d8ab657622a179f5ba4b3
SantGT5 <gianspf@gmail.com>
2022-01-07 16:04:19 +0100

    HotRefresh config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import "./styles.sass";
 import img from "./img.png";
-import logo from "./logo.svg"
+import logo from "./logo.svg";
+import { ClickCounter } from "./ClickCounter";
 
 export const App = () => {
   return (
     <div>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{" "}
+        {process.env.name}
+      </h1>
       <img src={img} alt="Logo" />
       <img src={logo} alt="Logo" />
+      <ClickCounter />
     </div>
   );
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,17 +1,19 @@
 const webpack = require("webpack");
-// const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+// hot refresh config
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
   mode: "development", // this indicate our app is in dev mode
   devServer: {
+    // hot refresh config
     hot: true,
     open: true,
   },
   devtool: "cheap-module-source-map",
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.name": JSON.stringify("Vishwas"),
+      "process.env.name": JSON.stringify("Gian"),
     }),
-    // new ReactRefreshWebpackPlugin(),
+    new ReactRefreshWebpackPlugin(),
   ],
 };
