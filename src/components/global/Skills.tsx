e24247dec87c8103fe137cefee590deb0c0bc7a3
SantGT5 <gianspf@gmail.com>
2022-01-11 23:02:03 +0100

    Gian Lucas Skills Icons && Styles

--- src/components/global/Skills.tsx
+++ src/components/global/Skills.tsx
@@ -5,9 +5,23 @@ export const Skills = () => {
     <div className="bg-color container">
       <h4 className="sub-title">APORTS</h4>
       <h1 className="title">MY SKILL</h1>
-      <div>
-        <SiReact size={120} color="#80888F" />
-        <p>React</p>
+      <div style={{ display: "flex", flexWrap: "nowrap" }}>
+        <div
+          style={{
+            backgroundColor: "red",
+            maxWidth: "120px",
+          }}
+        >
+          <SiReact size={120} color="#80888F" />
+          <p
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            React
+          </p>
+        </div>
       </div>
     </div>
   )
