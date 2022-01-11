46d9719119799252f3f912752bc45da1b60b2859
SantGT5 <gianspf@gmail.com>
2022-01-12 00:38:16 +0100

    Skills Marqee config

--- src/components/global/Skills.tsx
+++ src/components/global/Skills.tsx
@@ -1,27 +1,57 @@
-import { SiReact } from "react-icons/si"
+import Marquee from "react-fast-marquee"
+
+import {
+  SiReact,
+  SiNodedotjs,
+  SiTypescript,
+  SiJavascript,
+  SiNpm,
+  SiYarn,
+  SiCss3,
+  SiSass,
+  SiHtml5,
+  SiRedux,
+  SiJest,
+  SiWebpack,
+  SiFigma,
+  SiMongodb,
+  SiJson,
+} from "react-icons/si"
 
 export const Skills = () => {
+  const icons = [
+    { icon: SiReact, des: "React" },
+    { icon: SiNodedotjs, des: "NodeJS" },
+    { icon: SiTypescript, des: "TypeScript" },
+    { icon: SiJavascript, des: "JavaScript" },
+    { icon: SiNpm, des: "NPM" },
+    { icon: SiYarn, des: "Yarn" },
+    { icon: SiCss3, des: "CSS" },
+    { icon: SiSass, des: "SASS" },
+    { icon: SiHtml5, des: "HTML" },
+    { icon: SiRedux, des: "Redux" },
+    { icon: SiJest, des: "Jest" },
+    { icon: SiWebpack, des: "WebPack" },
+    { icon: SiFigma, des: "Figma" },
+    { icon: SiMongodb, des: "MongoDB" },
+    { icon: SiJson, des: "Json" },
+  ]
+
   return (
     <div className="bg-color container">
       <h4 className="sub-title">APORTS</h4>
       <h1 className="title">MY SKILL</h1>
-      <div style={{ display: "flex", flexWrap: "nowrap" }}>
-        <div
-          style={{
-            backgroundColor: "red",
-            maxWidth: "120px",
-          }}
-        >
-          <SiReact size={120} color="#80888F" />
-          <p
-            style={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            React
-          </p>
-        </div>
+      <div className="flex">
+        <Marquee speed={40} gradientColor={[248, 248, 248]}>
+          {icons.map((Icons, i) => {
+            return (
+              <div key={i} className="icons-container">
+                <Icons.icon className="skill-icons" />
+                <p className="center skill-des">{Icons.des}</p>
+              </div>
+            )
+          })}
+        </Marquee>
       </div>
     </div>
   )
